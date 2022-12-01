9844b89124e97ae87e99a268ab18eb5d53a5740f
Nazareno Cavazzon <cavazzonnazareno@gmail.com>
2022-12-01 14:43:01 -0300

    Decommented the 2 other mains

--- bin/main_production.dart
+++ bin/main_production.dart
@@ -1,24 +1,37 @@
-// import 'dart:async';
-// import 'dart:io';
+import 'dart:async';
+import 'dart:io';
 
-// import 'package:alcanza_backend/src/router.dart';
-// import 'package:alcanza_backend/src/utils/environment.dart';
-// import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf/shelf_io.dart' as io;
+import 'package:dotenv/dotenv.dart' as dotenv;
+import 'package:mongo_dart/mongo_dart.dart' as mongo;
+import 'package:weappear_backend/database/database.dart';
+import 'package:weappear_backend/src/router.dart';
+import 'package:weappear_backend/src/utils/utils.dart';
 
-// void main() {
-//   runZonedGuarded(
-//     () async {
-//       stdout.write(
-//           'Starting server at ${Environment().address}:${Environment().port}. \n');
-//       await io.serve(
-//         AlcanzaRouter().app,
-//         Environment().address,
-//         Environment().port,
-//         poweredByHeader: 'Powered by C-Ven',
-//       );
-//     },
-//     (error, stack) => stdout.write(
-//       'An error has occured:\n$error\n$stack',
-//     ),
-//   );
-// }
+void main() {
+  runZonedGuarded(
+    () async {
+      stdout.write('Starting server...\n');
+      final envFileExists = File.fromUri(Uri.parse('.env')).existsSync();
+      if (envFileExists) dotenv.load();
+
+      final db = await mongo.Db.create(Constants.mongoConnectionString);
+
+      final dbService = DatabaseService(db);
+
+      await dbService.open();
+
+      stdout.write('Connected to database.');
+      stdout.write('Starting server at localhost:8080. \n');
+      await io.serve(
+        WeAppearRouter().appPipeline,
+        'localhost',
+        8080,
+        poweredByHeader: 'WeAppear',
+      );
+    },
+    (error, stack) => stdout.write(
+      'An error has occured:\n$error\n$stack',
+    ),
+  );
+}
